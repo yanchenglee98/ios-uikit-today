e53d79ffca303b9aa66974c78f7a465470e31ec3
leeyancheng <leeyancheng@bigo.sg>
2023-06-13 10:19:05 +0800

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Lee Yan Cheng on 13/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
